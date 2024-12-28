f604eb32e8044bc79cef883fdd75d78032aae254
Roberto James <roberto.junior.james13@gmail.com>
2024-12-27 20:48:51 -0500

    starting navbar and create style folder

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
     /* Code by: Roberto James and Brandon Bent
      * 
      */
-    echo "Welcome to my Home Page";
-    echo "Telephone
-    "
 
 
 ?>
+
+
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NavBar</title>
+</head>
+<body>
+    <header>
+        <div id="navbar">
+            <div id="website_Name">HypMonitor</div>
+            <div>Home</div>
+            <div>Profile</div>
+            <div>Record BP</div>
+            <div>Support</div>
+            <div>About Us</div>
+        </div>
+    </header>
+</body>
+</html>
