98623448d3d88a7c470f2b4a9a78b9593421d46e
Roberto James <roberto.junior.james13@gmail.com>
2024-12-28 17:55:45 -0500

    add hover transitions to navbar icon and register/login option

--- navbar.php
+++ navbar.php
@@ -14,19 +14,20 @@
 
 <header>
     <link rel="stylesheet" href="<?php echo $level_down?>styles/indexStyle.css">
-    <div class="navbar">
-        <div class="website_Name">HypMonitor <img src="<?php echo $level_down ?>images/navbarImages/heart.svg" alt="Image of heart icon" > </div>
-        <div class="navMenu">
-            <div>Home</div>
+    <div id="navbar">
+        <div id="website_Name">HypMonitor <img src="<?php echo $level_down ?>images/navbarImages/heart.svg" alt="Image of heart icon" > </div>
+        <div id="navMenu">
+            <div id="home_option">Home</div>
             <div>Profile</div>
             <div>Record BP</div>
             <div>Support</div>
             <div>About Us</div>
         </div>
 
-        <div class="register_and_login_container">
-            <div class="register_container">Register</div> 
-            <div class="login_container">Login</div>
+        <div id="register_and_login_container">
+            <div id="register_container">Register</div> 
+            <div id="login_container">Login</div>
         </div>
     </div>
 </header>
+
