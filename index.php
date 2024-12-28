f69651fca25d033ca6b1017176c94fa58461b85c
Roberto James <roberto.junior.james13@gmail.com>
2024-12-27 23:11:07 -0500

    add content to nav bar

--- index.php
+++ index.php
@@ -6,4 +6,20 @@
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HomePage</title>
+    <link rel="stylesheet" href="styles/indexStyle.css">
+</head>
+<body>
+    <?php
+        require_once("navbar.php");
+    ?>
+
+    
+</body>
+</html>
 
--- navbar.php
+++ navbar.php
@@ -1,20 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NavBar</title>
-</head>
-<body>
-    <header>
-        <div id="navbar">
-            <div id="website_Name">HypMonitor</div>
+
+
+
+<header>
+    <div class="navbar">
+        <div class="website_Name">HypMonitor <img src="images/heart.svg" alt="" > </div>
+        <div class="navMenu">
             <div>Home</div>
             <div>Profile</div>
             <div>Record BP</div>
             <div>Support</div>
             <div>About Us</div>
         </div>
-    </header>
-</body>
-</html>
+
+        <div class="register_and_login_container">
+            <div class="register_container">Register</div> 
+            <div class="login_container">Login</div>
+        </div>
+    </div>
+</header>
