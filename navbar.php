23eb3520683322614bc41917494aef9cd0223a36
Roberto James <roberto.junior.james13@gmail.com>
2024-12-28 18:24:49 -0500

    1) add comments to navbar.php 2) define size for all columns on navbar

--- navbar.php
+++ navbar.php
@@ -3,8 +3,16 @@
     // Dynamically determine the base URL
     $base_url = dirname($_SERVER['SCRIPT_NAME']) .'/';
 
+    #determine how many levels from the root directiory the file is in by counting
+    #the amount of slashes and minus 2 eg: /Major_Project_DHI/. you minus two because by default the project
+    # folder will e included and sill have two slashes
     $level_down_count= (substr_count($base_url,'/')-2);
-    $level_down='';
+    $level_down='';#used to control how many level back we go
+    
+    /*this loop is used to determine how many levels back we go to reference the style
+    and image file. This makes referencing the files dynamic because the navbar file
+    will be referenced in multiple different pages which will be in various locations
+    */
     while($level_down_count!=0){
         $level_down_count--;
         $level_down = $level_down .'../';
@@ -15,13 +23,13 @@
 <header>
     <link rel="stylesheet" href="<?php echo $level_down?>styles/indexStyle.css">
     <div id="navbar">
-        <div id="website_Name">HypMonitor <img src="<?php echo $level_down ?>images/navbarImages/heart.svg" alt="Image of heart icon" > </div>
+        <div id="website_Name" onclick="window.location.href='index.php'">HypMonitor <img src="<?php echo $level_down ?>images/navbarImages/heart.svg" alt="Image of heart icon" > </div>
         <div id="navMenu">
-            <div id="home_option">Home</div>
+            <div id="home_option" onclick="window.location.href='index.php'">Home</div>
             <div>Profile</div>
             <div>Record BP</div>
             <div>Support</div>
-            <div>About Us</div>
+            <div onclick="window.location.href='about_us.php'">About Us</div>
         </div>
 
         <div id="register_and_login_container">
