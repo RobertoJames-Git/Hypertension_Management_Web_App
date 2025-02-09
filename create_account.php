9de1efd0ea337b0b59e3a025e615a0aa84d67ad4
Roberto James <roberto.junior.james13@gmail.com>
2025-02-09 01:09:27 -0500

    1) Added option form user to select their account in the form
    2) Remove reset button
    
    3) add styling for create account button

--- create_account.php
+++ create_account.php
@@ -7,27 +7,48 @@
     <link rel="stylesheet" href="styles/createAccount.css">
 </head>
 <body>
+
+    <?php 
+        require_once('navbar.php');
+    ?>
+
     <div id="form_container">
-        <form action="post" method="">
 
-            <label for="">First Name</label>
-            <input type="text" name="fname" id="">
-            <label for="">Last Name</label>
-            <input type="text" name="lname" id="">
-            <label for="">Date of Birth</label>
-            <input type="text" name="dob" id="">
-            <label for="">Email</label>
-            <input type="email" name="email" id="">
-            <label for="">Password</label>
-            <input type="password" name="pass">
+        <div>
+            <img src="images/CreateAccountImages/hyp_patient_and support.png" alt="" >
+        </div>
+        
+        <div>
 
-            <div id="button_container">
-                <input type="submit" value="Submit">
-                
-                <input type="reset" >
-            </div>
+            <h1> Create your account</h1>
+            
+            <form action="" method="post">
 
-        </form>
+                <label for="">First Name</label>
+                <input type="text" name="fname" id="">
+                <label for="">Last Name</label>
+                <input type="text" name="lname" id="">
+                <label for="">Date of Birth</label>
+                <input type="text" name="dob" id="">
+                <label for="">Select your account type</label>
+                <select name="user_type" id="">
+                    <option value="">Select an option</option>
+                    <option value="">Hypertensive Individual</option>
+                    <option value="">Family Member</option>
+                    <option value="">Healthcare Professional</option>
+                </select>
+                <label for="">Email</label>
+                <input type="email" name="email" id="">
+                <label for="">Password</label>
+                <input type="password" name="pass">
+
+                <br>
+                <input id="submit-btn" type="submit" value="Create Account">
+
+
+            </form>
+        </div>
+   
     </div>
 </body>
 </html>
